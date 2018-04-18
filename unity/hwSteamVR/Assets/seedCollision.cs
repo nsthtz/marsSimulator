109a2a68e424a4434cebbe5f0ba965c18fbf48b2
hellgoose <stiant92@gmail.com>
2018-04-18 17:21:55 +0200

    funksjonalitet

--- unity/hwSteamVR/Assets/seedCollision.cs
+++ unity/hwSteamVR/Assets/seedCollision.cs
@@ -8,8 +8,10 @@ public class seedCollision : MonoBehaviour
     private Animator anim1;
     private Animator anim2;
     private Animator anim3;
-    private bool closed;
-    private bool played = false;
+    public bool closed;
+    public bool played = false;
+    private TextPromptScript textPrompt;
+    private bool active;
 
     // Use this for initialization
     void Start()
@@ -17,14 +19,19 @@ public class seedCollision : MonoBehaviour
         anim1 = GameObject.Find("cabbage_model").GetComponent<Animator>();
         anim2 = GameObject.Find("cabbage2").GetComponent<Animator>();
         anim3 = GameObject.Find("cabbage3").GetComponent<Animator>();
+        textPrompt = GameObject.Find("Player").GetComponent<TextPromptScript>();
+        active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Hydroponic = GameObject.Find("Lid").GetComponent<Hydroponic>();
-        if (closed && Hydroponic.closed && !played)
+        // Debug.Log(closed + " " + textPrompt.active + " ");
+        if (closed && textPrompt.active && !played)
         {
+            
             anim1.SetTrigger("GoCabbage");
             anim2.SetTrigger("GoCabbage");
             anim3.SetTrigger("GoCabbage");
@@ -35,7 +42,7 @@ public class seedCollision : MonoBehaviour
 
     void OnCollisionEnter(Collision collisionInfo)
     {
-        Debug.Log("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+     //   Debug.Log("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
         if (collisionInfo.collider.tag == "Seed")
         {
             closed = true;
